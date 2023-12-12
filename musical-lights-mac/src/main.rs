1bb0bf6abe8d311e4a63fd031c7fa0866c2764b4
Bryan Stitt <bryan@stitthappens.com>
2023-12-11 21:12:08 -0800

    simple fft

--- musical-lights-mac/src/main.rs
+++ musical-lights-mac/src/main.rs
@@ -1,13 +1,11 @@
-use std::sync::mpsc::Sender;
+//! TODO: refactor this to use the types in microphone.rs
 
-use cpal::{
-    traits::{DeviceTrait, HostTrait, StreamTrait},
-    Sample,
-};
+use std::time::Duration;
 
-fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use microfft::real::rfft_512;
 
+fn record_microphone(x: Duration) -> anyhow::Result<()> {
     let host = cpal::default_host();
 
     // TODO: host.input_devices()?.find(|x| x.name().map(|y| y == opt.device).unwrap_or(false))
@@ -21,30 +19,28 @@ fn main() -> anyhow::Result<()> {
         eprintln!("an error occurred on stream: {}", err);
     };
 
-    let fft = ();
-
     let stream = match config.sample_format() {
-        cpal::SampleFormat::I8 => device.build_input_stream(
-            &config.into(),
-            move |data, _: &_| write_input_data::<i8>(data, &fft),
-            err_fn,
-            None,
-        )?,
-        cpal::SampleFormat::I16 => device.build_input_stream(
-            &config.into(),
-            move |data, _: &_| write_input_data::<i16>(data, &fft),
-            err_fn,
-            None,
-        )?,
-        cpal::SampleFormat::I32 => device.build_input_stream(
-            &config.into(),
-            move |data, _: &_| write_input_data::<i32>(data, &fft),
-            err_fn,
-            None,
-        )?,
+        // cpal::SampleFormat::I8 => device.build_input_stream(
+        //     &config.into(),
+        //     move |data, _: &_| write_input_data::<i8>(data, &fft),
+        //     err_fn,
+        //     None,
+        // )?,
+        // cpal::SampleFormat::I16 => device.build_input_stream(
+        //     &config.into(),
+        //     move |data, _: &_| write_input_data::<i16>(data, &fft),
+        //     err_fn,
+        //     None,
+        // )?,
+        // cpal::SampleFormat::I32 => device.build_input_stream(
+        //     &config.into(),
+        //     move |data, _: &_| write_input_data::<i32>(data, &fft),
+        //     err_fn,
+        //     None,
+        // )?,
         cpal::SampleFormat::F32 => device.build_input_stream(
             &config.into(),
-            move |data, _: &_| write_input_data::<f32>(data, &fft),
+            |data, _: &_| write_input_data(data),
             err_fn,
             None,
         )?,
@@ -57,16 +53,38 @@ fn main() -> anyhow::Result<()> {
 
     stream.play()?;
 
-    // Let recording go for roughly three seconds.
-    std::thread::sleep(std::time::Duration::from_secs(1));
+    // record for x seconds
+    std::thread::sleep(x);
     drop(stream);
 
     Ok(())
 }
 
-fn write_input_data<T>(input: &[T], fft: &())
-where
-    T: Sample,
-{
+fn main() -> anyhow::Result<()> {
+    println!("Hello, world!");
+
+    // TODO: multiple threads
+    record_microphone(Duration::from_secs(10))?;
+
+    Ok(())
+}
+
+fn write_input_data(input: &[f32]) {
     println!("heard {} samples", input.len());
+
+    assert_eq!(input.len(), 512);
+
+    let mut input: [f32; 512] = input[..512].try_into().unwrap();
+
+    let spectrum = rfft_512(&mut input);
+
+    // since the real-valued coefficient at the Nyquist frequency is packed into the
+    // imaginary part of the DC bin, it must be cleared before computing the amplitudes
+    spectrum[0].im = 0.0;
+
+    let amplitudes: Vec<_> = spectrum.iter().map(|c| c.norm() as u32).collect();
+
+    println!("sum amplitudes: {:?}", amplitudes);
+
+    // TODO: what should we do with the amplitudes? send them through a channel?
 }
