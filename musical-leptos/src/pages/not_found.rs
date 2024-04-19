d45fcf8b149f4a4685ef4a14c9163958c8b3a3b8
Bryan Stitt <bryan@stitthappens.com>
2024-04-18 19:29:45 -0700

    use public url

--- musical-leptos/src/pages/not_found.rs
+++ musical-leptos/src/pages/not_found.rs
@@ -3,5 +3,7 @@ use leptos::*;
 /// 404 Not Found Page
 #[component]
 pub fn NotFound() -> impl IntoView {
+    // TODO: console.log errors
+
     view! { <h1>"Uh oh!" <br/> "We couldn't find that page!"</h1>  }
 }
