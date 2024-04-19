bb5438f83c4eedd1384451ac4beb1c7935b72147
Bryan Stitt <bryan@stitthappens.com>
2024-04-18 22:25:17 -0700

    remove obvious comments

--- musical-leptos/src/lib.rs
+++ musical-leptos/src/lib.rs
@@ -23,10 +23,8 @@ pub fn App() -> impl IntoView {
     view! {
         <Html lang="en" dir="ltr" attr:data-theme="light"/>
 
-        // sets the document title
         <Title text="Musical Leptos CSR"/>
 
-        // injects metadata in the <head> of the page
         <Meta charset="UTF-8"/>
         <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
