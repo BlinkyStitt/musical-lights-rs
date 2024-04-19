2b0ecdeb05a815d61e38f538d47a820732ecffe9
Bryan Stitt <bryan@stitthappens.com>
2024-04-18 20:10:45 -0700

    always render home

--- musical-leptos/src/lib.rs
+++ musical-leptos/src/lib.rs
@@ -27,11 +27,14 @@ pub fn App() -> impl IntoView {
         <Meta charset="UTF-8"/>
         <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-        <Router>
-            <Routes>
-                <Route path="/" view=Home/>
-                <Route path="/*" view=NotFound/>
-            </Routes>
-        </Router>
+        // <Router>
+        //     <Routes>
+        //         <Route path="/" view=Home/>
+        //         <Route path="/*" view=NotFound/>
+        //     </Routes>
+        // </Router>
+
+        // TODO: figure out how to get the router working on github pages. i thought public url would do it
+        <Home />
     }
 }
