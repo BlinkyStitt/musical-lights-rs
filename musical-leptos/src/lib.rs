9fcd3872ed58cc0ceba66204fea5341f24063ab0
Bryan Stitt <bryan@stitthappens.com>
2024-04-21 22:53:23 -0700

    og meta tags

--- musical-leptos/src/lib.rs
+++ musical-leptos/src/lib.rs
@@ -23,10 +23,16 @@ pub fn App() -> impl IntoView {
     view! {
         <Html lang="en" dir="ltr" attr:data-theme="light"/>
 
-        <Title text="Musical Leptos CSR"/>
+        <Title text="Musical Lights"/>
 
         <Meta charset="UTF-8"/>
         <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <Meta name="description" content="Make some lights blink to your microphone."/>
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://blink.stitthappens.com" />
+        <meta property="og:site_name" content="Stitt Happens">
+        <meta property="og:locale" content="en_US">
 
         <div class="container">
             <Router fallback=move || NotFound().into_view()>
