b037f4abef33835c295489614b2732d156581833
Bryan Stitt <bryan@stitthappens.com>
2025-05-10 22:44:13 -0700

    add fps

---
+++ musical-adafruit-sparkle/src/fps.rs
@@ -0,0 +1,35 @@
+use defmt::info;
+use embassy_time::Instant;
+
+pub struct FpsTracker {
+    last: Instant,
+    count: u32,
+}
+
+impl Default for FpsTracker {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl FpsTracker {
+    pub fn new() -> Self {
+        Self {
+            last: Instant::now(),
+            count: 0,
+        }
+    }
+
+    pub fn tick(&mut self) {
+        self.count += 1;
+        let now = Instant::now();
+        let elapsed = now.duration_since(self.last);
+
+        if elapsed.as_millis() >= 1000 {
+            let fps = self.count;
+            self.count = 0;
+            self.last = now;
+            info!("FPS: {}", fps);
+        }
+    }
+}
